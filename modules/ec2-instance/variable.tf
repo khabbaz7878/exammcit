78fddd91e76a75418d76d35827122160c9dab388
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-02-08 21:54:05 -0500

    Update variable.tf

--- modules/ec2-instance/variable.tf
+++ modules/ec2-instance/variable.tf
@@ -1,24 +1,11 @@
-variable "instance_name" {
-  type        = string
-  description = "Name of the EC2 instance"
-}
-
 variable "ami" {
-  type        = string
   description = "AMI ID for the EC2 instance"
 }
 
 variable "instance_type" {
-  type        = string
   description = "Instance type for the EC2 instance"
 }
 
-variable "vpc_id" {
-  type        = string
-  description = "VPC ID for the EC2 instance"
-}
-
 variable "subnet_id" {
-  type        = string
-  description = "Subnet ID for the EC2 instance"
+  description = "Subnet ID where the EC2 instance will be launched"
 }
