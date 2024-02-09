d6a86112985413fe7b70f820d3d47014d9e5ecc2
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-02-08 21:04:26 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,6 @@
-provider "aws" {
-  region = "us-west-2"
-}
+module "vpc" {
+  source = "./modules/vpc"
 
-module "vpcmodule" {
-  source = "./module/vpcmodule"
-
-  cidr_block      = "10.0.0.0/16"
-  vpc_name        = "my_vpc"
-  subnet_cidr_block = "10.0.1.0/24"
-  subnet_name      = "my_subnet"
-}
-
-module "ec2module" {
-  source = "./module/ec2module"
-
-  ami             = "ami-0c55b159cbfafe1f0" # This is an example; replace it with the correct AMI ID for your region
-  instance_type   = "t2.micro"
-  subnet_id       = module.vpc_subnet.subnet_id
-  instance_name   = "my_ec2_instance"
-}
+  name               = "my-vpc"
+  cidr_block         = "10.0.0.0/16"
+  subnet_cidr_
