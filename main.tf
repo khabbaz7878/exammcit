b6b18d8900e0a7198c9710d409b587186bf39b0b
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-02-08 22:12:34 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,19 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-east-1"
-}
+# main.tf
 
 module "vpc" {
-  source = "./vpc_module"
+  source = "./modules/vpc"
+
+  name       = "example-vpc"
+  cidr_block = "10.0.0.0/16"
 }
 
-module "ec2_instance" {
-  source   = "./ec2_instance"
-  vpc_id   = module.vpc.vpc_id
-  subnet_id = module.vpc.subnet_id
+module "ec2" {
+  source = "./modules/ec2"
+
+  name              = "example-ec2"
+  ami               = "ami-12345678"
+  instance_type     = "t2.micro"
+  instance_name     = "example-ec2"
+  subnet_id         = module.vpc.subnet_id
+  vpc_id            = module.vpc.vpc_id
 }
