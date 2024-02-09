a2630c9f7ba55e83c310bd52c8029ea23bd8944a
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-02-08 21:16:53 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ module "vpc" {
   subnet_cidr_block  = "10.0.1.0/24"
 }
 
-module "ec2_instance" {
-  source = "./modules/ec2"
+module "ec2-instance" {
+  source = "./modules/ec2-instance"
 
   instance_name   = "my-ec2-instance"
   ami             = "ami-0c55b159cbfafe1f0" # Replace this
