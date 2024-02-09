0c0166be4aa2bc3e3c153082d1f6100b1e0916cc
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-02-08 21:12:52 -0500

    Update main.tf

--- modules/ec2-instance/main.tf
+++ modules/ec2-instance/main.tf
@@ -1,39 +1,45 @@
-data "aws_vpc" "this" {
+data "aws_vpc" "selected" {
   id = var.vpc_id
 }
 
-resource "aws_instance" "this" {
+resource "aws_instance" "instance" {
   ami           = var.ami
   instance_type = var.instance_type
   subnet_id     = var.subnet_id
 
   vpc_security_group_ids = [aws_security_group.instance.id]
 
+  user_data = file("install-httpd.sh")
+
   tags = {
-    Name = var.name
+    Name = var.instance_name
   }
 }
 
 resource "aws_security_group" "instance" {
-  name_prefix = var.name
-  description = "Security group for the EC2 instance."
-  vpc_id      = data.aws_vpc.this.id
+  name_prefix = var.instance_name
+  description = "Security group for the EC2 instance"
+  vpc_id      = data.aws_vpc.selected.id
 
   ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
-    cidr_blocks = [
-      "0.0.0.0/0"
-    ]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-    cidr_blocks = [
-      "0.0.0.0/0"
-    ]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+output "instance_id" {
+  value = aws_instance.instance.id
+}
+
+output "instance_public_ip" {
+  value = aws_instance.instance.public_ip
 }
