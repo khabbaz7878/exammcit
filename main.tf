1e13ed8146397e2956643acefa5815b241fc7370
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-02-08 21:45:46 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,25 @@
 module "vpc" {
-  source = "./modules/vpc"
+  source = "./vpc"
 
   name               = "my-vpc"
   cidr_block         = "10.0.0.0/16"
-  subnet_cidr_block  = "10.0.1.0/24"
 }
 
-module "ec2-instance" {
-  source = "./modules/ec2-instance"
+module "ec2" {
+  source = "./ec2-instance"
 
-  instance_name   = "my-ec2-instance"
-  ami             = "ami-0c55b159cbfafe1f0" # Replace this
-
+  name              = "my-ec2-instance"
+  ami               = "ami-0c55b159cbfafe1f0"
+  instance_type     = "t2.micro"
+  vpc_id            = module.vpc.vpc_id
+  subnet_id         = aws_subnet.public.id
 }
 
+resource "aws_subnet" "public" {
+  vpc_id     = module.vpc.vpc_id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Public Subnet"
+  }
+}
