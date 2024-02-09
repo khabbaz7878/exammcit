b04aae613f248a6e5bb2db0e9bb5956ce6a68067
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-02-08 21:53:43 -0500

    Update main.tf

--- modules/ec2-instance/main.tf
+++ modules/ec2-instance/main.tf
@@ -1,10 +1,6 @@
-module "ec2_instance" { 
-source = "terraform-aws-modules/ec2-instance/aws"
-
-name = "my-instance" ami = "ami-ebd02392" 
-instance_type = "t2.micro" 
-vpc_security_group_ids = [module.vpc.default_security_group_id] 
-subnet_id = module.vpc.public_subnets[0]
-
-tags = { Name = "My EC2 Instance" }
+resource "aws_instance" "example" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+  # Add other instance configurations as needed
 }
