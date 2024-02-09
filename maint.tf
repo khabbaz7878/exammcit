d90d71ba996275e02e859eecc9912a55e2254d07
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-02-08 20:08:32 -0500

    Update maint.tf

--- maint.tf
+++ maint.tf
@@ -1,15 +1,13 @@
-provider "aws" {
-  region = var.aws_region
-}
+# provider "aws" {
+#   region = var.aws_region
+# }
 
-locals {
-  instance_ami = "ami-0c55b159cbfafe1f0"
-  instance_http_port = 80
-}
+# module "vpc" {
+#   source  = "terraform-aws-modules/vpc/aws"
+#   version = "3.0.0"
 
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "3.0.0"
+#   name = "my-vpc"
+#   cidr = var.vpc_cidr
+# }
 
-  name = "my-vpc"
-  cidr = var.vpc_cid
+# ... rest of your main.tf file ...
