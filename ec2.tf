9211930c4e70cd2737266f59964998628784cc80
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-02-08 20:11:39 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,47 +1,8 @@
-data "aws_vpc" "selected" {
-  filter {
-    name   = "tag:Name"
-    values = ["my-vpc"]
-  }
-}
+# ec2_instance_module/main.tf
 
-data "aws_subnet_ids" "public" {
-  vpc_id = data.aws_vpc.selected.id
-
-  tags = {
-    Tier = "public"
-  }
-}
-
-resource "aws_instance" "example" {
-  ami           = var.instance_ami
-  instance_type = var.instance_type
-  key_name      = "example-key"
-  subnet_id     = tolist(data.aws_subnet_ids.public.ids)[0]
-
-  vpc_security_group_ids = [aws_security_group.instance.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World!" > index.html
-              nohup busybox httpd -f -p "${var.instance_http_port}" &
-              EOF
-
-  tags = {
-    Name = "example-instance"
-  }
-}
-
-resource "aws_security_group" "instance" {
-  name_prefix = "example-instance"
-
-  ingress {
-    from_port = var.instance_http_port
-    to_port   = var.instance_http_port
-    protocol  = "tcp"
-
-    cidr_blocks = [
-      "0.0.0.0/0",
-    ]
-  }
+resource "aws_instance" "my_instance" {
+  ami           = "ami-12345678"  # Replace with your desired AMI
+  instance_type = "t2.micro"
+  subnet_id     = var.subnet_id   # Using subnet_id from input variables
+  # Add other instance configurations as needed
 }
