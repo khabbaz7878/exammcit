9dc8250e4ee914a4bc04ad0918532b0a36959f4c
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-02-08 20:03:16 -0500

    Create maint.tf

---
+++ maint.tf
@@ -0,0 +1,25 @@
+# VPC module
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+}
+
+# EC2 module
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  
+  name = "my-ec2-instance"
+
+  ami                    = "ami-ebd02392"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id              = module.vpc.public_subnets[0]
+}
