6d470cebd6c09ba7573c1ce3272543593055c8f8
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-02-08 20:14:38 -0500

    Update maint.tf

--- maint.tf
+++ maint.tf
@@ -1,12 +1,10 @@
 # main.tf
 
-module "my_vpc" {
-  source = "./vpc_module"
-  # Add any input variables for VPC module if needed
+module "vpc" {
+  source = "./vpc"
 }
 
-module "my_ec2_instance" {
-  source    = "./ec2_instance_module"
-  subnet_id = module.my_vpc.subnet_a_id  # Pass the subnet ID from VPC module output
-  # Add any other input variables for EC2 module if needed
+module "ec2" {
+  source = "./ec2"
+  vpc_id = module.vpc.vpc_id
 }
