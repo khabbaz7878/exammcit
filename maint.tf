19b7a9efd03ed6913fd6f88da424486c196d8144
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-02-08 20:11:11 -0500

    Update maint.tf

--- maint.tf
+++ maint.tf
@@ -1,13 +1,12 @@
-# provider "aws" {
-#   region = var.aws_region
-# }
+# main.tf
 
-# module "vpc" {
-#   source  = "terraform-aws-modules/vpc/aws"
-#   version = "3.0.0"
+module "my_vpc" {
+  source = "./vpc_module"
+  # Add any input variables for VPC module if needed
+}
 
-#   name = "my-vpc"
-#   cidr = var.vpc_cidr
-# }
-
-# ... rest of your main.tf file ...
+module "my_ec2_instance" {
+  source    = "./ec2_instance_module"
+  subnet_id = module.my_vpc.subnet_a_id  # Pass the subnet ID from VPC module output
+  # Add any other input variables for EC2 module if needed
+}
