c3c70a6943d04ff21a529a53308f7a4296b6eadd
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-02-08 20:57:33 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,44 +1,21 @@
-# VPC module
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "my-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
-  }
+provider "aws" {
+  region = "us-west-2"
 }
 
-# Public subnets
-resource "aws_subnet" "public" {
-  vpc_id = module.vpc.vpc_id
-  cidr_block = "10.0.101.0/24"
-  availability_zone = "us-east-1a"
+module "vpc_subnet" {
+  source = "./modules/vpc_subnet"
 
-  tags = {
-    Name = "Public subnet 1"
-  }
+  cidr_block      = "10.0.0.0/16"
+  vpc_name        = "my_vpc"
+  subnet_cidr_block = "10.0.1.0/24"
+  subnet_name      = "my_subnet"
 }
 
-# EC2 instance
 module "ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
+  source = "./modules/ec2_instance"
 
-  name = "my-ec2-instance"
-
-  ami                    = "ami-ebd02392"
-  instance_type          = "t2.micro" 
-  vpc_security_group_ids = [module.vpc.default_security_group_id]
-  subnet_id              = aws_subnet.public.id
-
-  tags = {
-    Terraform   = "true"
-    Environment = "dev" 
-  }
+  ami             = "ami-0c55b159cbfafe1f0" # This is an example; replace it with the correct AMI ID for your region
+  instance_type   = "t2.micro"
+  subnet_id       = module.vpc_subnet.subnet_id
+  instance_name   = "my_ec2_instance"
 }
